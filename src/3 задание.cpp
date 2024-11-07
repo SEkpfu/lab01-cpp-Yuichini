ca7b9d20a6fa3f3b2ce3398c03c0872f4b27d4ed
Yuichini <ulaovcinnikova690@gmail.com>
2024-11-07 22:27:01 +0300

    Add files via upload

---
+++ src/3 задание.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream> 
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x; 
+    std::cout << "Введите значение x: ";
+    std::cin >> x;
+    double x2 = x * x;         
+    double x3 = x2 * x;       
+    double x4 = x3 * x;       
+    double y = 5 * x4 + 2 * x3 - 7 * x2 + 3 * x + 4; 
+    std::cout << "Результат y: " << y << "\n";
+}
