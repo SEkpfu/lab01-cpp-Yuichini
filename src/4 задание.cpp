bfbdbe5785ddf8028e100bd461a4841fcb6aa73b
Yuichini <ulaovcinnikova690@gmail.com>
2024-11-08 00:00:34 +0300

    Add files via upload

---
+++ src/4 задание.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    setlocale(LC_ALL, "RU")
+    double x, p;
+    std::cout << "Введите значение x: ";
+    std::cin >> x;
+    if (x <= -10) {
+        p = (12.0 / 7.0) * std::pow(5, 12);
+    }
+    else if (x > -10 && x <= 0) {
+        p = sqrt(10 - std::pow(x, 2));
+    }
+    else {
+        p = cos(M_PI * (x + 1));
+    }
+    std::cout << "Значение p: " << p << "\n";
+    return 0;
+}
+
