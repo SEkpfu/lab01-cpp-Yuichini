9a34a30d55957f1c536ff9e32f2b0812b0a3a2be
Yuichini <ulaovcinnikova690@gmail.com>
2024-11-08 20:48:38 +0300

    Add files via upload

---
+++ src/6 задание.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите n: ";
+    cin >> n;
+    double s = 0; 
+    double sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += 2 * i; 
+        s += cos(sum);
+    }
+    cout << "Результат: " << s << "\n";
+    return 0;
+}
